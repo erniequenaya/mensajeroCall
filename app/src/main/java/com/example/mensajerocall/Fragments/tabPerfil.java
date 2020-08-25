49d99339dfe7a640940e1e398f9245b7ee78738e
seishi1 <major_seishi@hotmail.com>
2020-08-25 04:07:44 -0400

    added every functionality but Fragment changing

--- app/src/main/java/com/example/mensajerocall/Fragments/tabPerfil.java
+++ app/src/main/java/com/example/mensajerocall/Fragments/tabPerfil.java
@@ -1,12 +1,16 @@
 package com.example.mensajerocall.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.mensajerocall.R;
 
@@ -25,6 +29,28 @@ public class tabPerfil extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_tab_perfil, container, false);
+
+        ImageButton ibProfCall = (ImageButton) view.findViewById(R.id.ibProfileCall);
+        ibProfCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent profCall = new Intent();
+                profCall.setAction(Intent.ACTION_DIAL);
+                v.getContext().startActivity(profCall);
+            }
+        });
+
+        //b
+
+        ImageButton ibProfCam = (ImageButton) view.findViewById(R.id.ibVideoCall);
+        ibProfCam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent profCam = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                v.getContext().startActivity(profCam);
+            }
+        });
+
         return view;
     }
 }
